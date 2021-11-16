2d56335c16a4141f00db667589e965314eb75491
BurriVik <burrivickram@gmail.com>
2021-11-16 10:59:58 +0530

    first commit

---
+++ spring-batch-with-spring-boot/src/main/java/com/coder/springbatchwithspringboot/SpringBatchWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.coder.springbatchwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchWithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchWithSpringBootApplication.class, args);
+	}
+
+}
---
+++ spring-batch-with-spring-boot/src/test/java/com/coder/springbatchwithspringboot/SpringBatchWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coder.springbatchwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
