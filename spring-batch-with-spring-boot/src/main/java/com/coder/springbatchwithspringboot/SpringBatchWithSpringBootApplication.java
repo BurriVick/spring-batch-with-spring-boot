3081b1df53403650e9676df9d36a6b921c344921
BurriVik <burrivickram@gmail.com>
2021-11-16 11:51:31 +0530

    add comments

--- spring-batch-with-spring-boot/src/main/java/com/coder/springbatchwithspringboot/SpringBatchWithSpringBootApplication.java
+++ spring-batch-with-spring-boot/src/main/java/com/coder/springbatchwithspringboot/SpringBatchWithSpringBootApplication.java
@@ -3,6 +3,11 @@ package com.coder.springbatchwithspringboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Vikram Reddy Burri
+ * @mail : vickramburri@gmail.com
+ *
+ */
 @SpringBootApplication
 public class SpringBatchWithSpringBootApplication {
 
